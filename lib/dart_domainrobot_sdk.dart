d647ab7031d6f66806d16d6f67ee8a13a89fd908
Ephenodrom <30625794+Ephenodrom@users.noreply.github.com>
2020-06-05 10:19:44 +0200

    #6

--- lib/dart_domainrobot_sdk.dart
+++ lib/dart_domainrobot_sdk.dart
@@ -4,3 +4,4 @@ export 'src/model/models_lib.dart';
 export 'src/model/exception/exception_lib.dart';
 export 'src/DomainRobot.dart';
 export 'src/DomainRobotHeaders.dart';
+export 'src/clients/AbstractDomainRobotClient.dart';
